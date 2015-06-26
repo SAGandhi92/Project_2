dee651a823b4224de681ac135bb37f582de9ce4b
Salonee Gandhi <gandhin92@gmail.com>
2015-06-26 15:37:17 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,9 @@
+var express         = require('express'),
+    server          = require(),
+    ejs             = require('ejs'),
+    bodyParser      = require(body-parser),
+    morgan          = require('morgan'),
+    mongoose        = require('mongoose'),
+    methodOverride = require('method-override'),
+    expressLayout   = require('express-ejs-layouts');
+    
